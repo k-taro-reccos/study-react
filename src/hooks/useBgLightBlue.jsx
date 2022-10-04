9380a97c139293627958f9858a1ee5f9f777fd2d
k-taro-reccos <tarou3806@gmail.com>
2022-10-04 20:41:12 +0900

    🎨 Custom Hooksを使ってうまくまとめました。

---
+++ src/hooks/useBgLightBlue.jsx
@@ -0,0 +1,11 @@
+const { useEffect } = require("react");
+
+export const useBgLightBlue = () => {
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue";
+
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
+};
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,51 +1,16 @@
 import Head from "next/head";
-import { useCallback, useEffect, useState } from "react";
 import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
+import { useBgLightBlue } from "src/hooks/useBgLightBlue";
+import { useCounter } from "src/hooks/useCounter";
+import { useInputArray } from "src/hooks/useInputArray";
 import styles from "src/styles/Home.module.css";
 
 export default function Home() {
-  const [count, setCount] = useState(1);
-  const [text, setText] = useState("");
-  const [isShow, setIsShow] = useState(true);
-  const [array, setArray] = useState([]);
-
-  const handleClick = useCallback(() => {
-    if (count < 10) {
-      setCount((prevCount) => prevCount + 1);
-    }
-  }, [count]);
-
-  const handleDisplay = useCallback(() => {
-    setIsShow((prevIsShow) => !prevIsShow);
-  }, []);
-
-  const handleChange = useCallback((e) => {
-    if (e.target.value.length > 5) {
-      alert("5文字以内にしてください");
-      return;
-    }
-    setText(e.target.value.trim());
-  }, []);
-
-  const handleAdd = useCallback(() => {
-    setArray((prevArray) => {
-      if (prevArray.some((item) => item === text)) {
-        alert("同じ要素がすでに存在します");
-        return prevArray;
-      }
-      return [...prevArray, text];
-    });
-  }, [text]);
-
-  useEffect(() => {
-    document.body.style.backgroundColor = "lightblue";
-
-    return () => {
-      document.body.style.backgroundColor = "";
-    };
-  }, []);
+  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { text, array, handleChange, handleAdd } = useInputArray();
+  useBgLightBlue();
 
   return (
     <div className={styles.container}>
@@ -55,10 +20,12 @@ export default function Home() {
       </Head>
 
       <Header />
+
       {isShow && <p>{count}</p>}
       <button onClick={handleClick}>ボタン</button>
       <br />
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+
       <br />
       <input type="text" value={text} onChange={handleChange} />
       <br />
