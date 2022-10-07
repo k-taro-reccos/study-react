e92ad8e0c3cb1d7e8a38a0f3f85b88a04a221be3
k-taro-reccos <tarou3806@gmail.com>
2022-10-07 19:39:37 +0900

    🚚 useFetchArrayのhooksをわかりやすように並び替え

--- src/hooks/useFetchArray.jsx
+++ src/hooks/useFetchArray.jsx
@@ -14,26 +14,25 @@ const useFetchArray = (url) => {
 
 const API_URL = "https://jsonplaceholder.typicode.com"
 
-export const useComments = () => {
-  return useFetchArray(`${API_URL}/comments`)
-}
-
+// posts
 export const usePosts = () => {
   return useFetchArray(`${API_URL}/posts`)
-}
-
-export const useUsers = () => {
-  return useFetchArray(`${API_URL}/users`)
-}
-
-export const useCommentsByPostId = (id) => {
-  return useFetchArray(id ? `${API_URL}/comments?postId=${id}` : null)
 }
 
 export const usePostsByUserId = (id) => {
   return useFetchArray(id ? `${API_URL}/posts?userId=${id}` : null)
 }
 
-export const useUsersById = (id) => {
-  return useFetchArray(id ? `${API_URL}/users?id=${id}` : null)
+// users
+export const useUsers = () => {
+  return useFetchArray(`${API_URL}/users`)
+}
+
+// comments
+export const useComments = () => {
+  return useFetchArray(`${API_URL}/comments`)
+}
+
+export const useCommentsByPostId = (id) => {
+  return useFetchArray(id ? `${API_URL}/comments?postId=${id}` : null)
 }
