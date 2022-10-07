293b35feb37c91c8ea72d9f6ab3774250b7d2334
k-taro-reccos <tarou3806@gmail.com>
2022-10-07 23:06:12 +0900

    💄 Tailwind CSSを使って既存のスタイルを書き換え

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,7 +1,5 @@
-import classes from "src/components/Layout/Layout.module.css";
-
 export const Layout = (props) => {
   const { children } = props;
 
-  return <div className={classes.container}>{children}</div>;
+  return <div className="flex flex-col items-center mx-auto max-w-2xl px-2 min-h-screen">{children}</div>;
 };
